af37bbcb5d7a876d90dc4666b6fc9530df835fcf
Dan Sabadis <dan_sabadas@hotmail.com>
2017-10-04 14:56:17 +0300

    added DB migration

---
+++ DataLayer/SalesContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using Model;
+
+namespace DataLayer
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext() : base("DefaultConnection")
+        {            
+        }
+
+
+        public DbSet<SalesOrder> SalesOrders { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configurations.Add(new SalesOrderConfiguration());
+        }
+    }
+}
---
+++ DataLayer/SalesOrderConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Model;
+
+namespace DataLayer
+{
+    public class SalesOrderConfiguration : EntityTypeConfiguration<SalesOrder>
+    {
+        public SalesOrderConfiguration()
+        {
+            Property(so => so.CustomerName).HasMaxLength(30).IsRequired();
+            Property(so => so.PONumber).HasMaxLength(10).IsOptional();
+        }
+    }
+}
